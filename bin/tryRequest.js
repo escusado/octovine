b2d23cec37d8e0a7682ff9239e1e94bc7fd288ca
Toily <escusado@gmail.com>
2015-05-13 00:29:46 -0500

    check filename

--- bin/tryRequest.js
+++ bin/tryRequest.js
@@ -1,21 +1,24 @@
 //printercommand: node /home/pi/apps/octovine/bin/saveMeta.js --data "command|%(data)s|%(filename)s|%(progress)s|%(currentZ)s|%(now)s"
 
-var exec = require('child_process').exec,
-  path = require('path'),
-  sh = require('execSync'),
-  storage = path.resolve('./storage'),
-  Finder = require('fs-finder'),
-  removeCommand = 'rm '+storage+'/*',
-  server = 'http://192.168.100.7:3000/';
+var filename,
+    exec = require('child_process').exec,
+    path = require('path'),
+    sh = require('execSync'),
+    storage = path.resolve('./storage'),
+    Finder = require('fs-finder'),
+    removeCommand = 'rm '+storage+'/*',
+    server = 'http://192.168.100.7:3000/',
 
   files = Finder.in(storage).findFiles();
-  filename = path.basename(files[0])
 
-exec('curl '+server+filename, function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }
-    sh.run(removeCommand);
-});
+if (files[0]) {
+  filename = path.basename(files[0]);
+  exec('curl '+server+filename, function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+      sh.run(removeCommand);
+  });
+}
